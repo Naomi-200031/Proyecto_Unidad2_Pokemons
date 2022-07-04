1ce6d2200710548e1ab9a3a7ebbb5ec94b095334
Naomi-200031 <79290699+Naomi-200031@users.noreply.github.com>
2022-07-04 17:39:19 -0500

    feat: I adding an another change

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import App from "./App";
+import GetIn from "./navbar";
 
 const  First= () => {
     return (
         <div>
+            <GetIn />
             <BrowserRouter>
                 <Routes>
                     <Route path="/homepokemons" element={<App />}></Route>
